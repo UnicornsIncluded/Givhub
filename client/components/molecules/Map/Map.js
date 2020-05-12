a7754081c129393a54c2da94d0a5379a30f42dde
Tyler Eaden <tyler.eaden@gmail.com>
2020-05-12 07:54:47 -0400

    Feature: Added GeoLocation To Mapbox Map

--- client/components/molecules/Map/Map.js
+++ client/components/molecules/Map/Map.js
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
-import MapGL from "react-map-gl"
+import MapGL, { GeolocateControl } from "react-map-gl"
 import "mapbox-gl/dist/mapbox-gl.css"
 
+const geolocateStyle = {
+    float: 'left',
+    margin: '50px',
+    padding: '10px'
+};
+
 export default function Map() {
-    const [viewport, setViewport] = useState({
+    const [viewport, setViewPort] = useState({
         latitude: 40.705,
         longitude: -74.009,
         width: "100vw",
@@ -11,13 +17,21 @@ export default function Map() {
         zoom: 12,
     })
 
+    const _onViewportChange = viewport => setViewPort({ ...viewport })
+
     return (
         <div>
             <MapGL
                 {...viewport}
                 mapboxApiAccessToken={"pk.eyJ1IjoidGVhZGVuIiwiYSI6ImNrNXdwbGFwYjE1OHYzbW14YTllZmdzb3MifQ.0hqWN7w_oxX7qzJ5w30EfQ"}
                 mapStyle="mapbox://styles/mapbox/streets-v10"
+                onViewportChange={_onViewportChange}
             >
+                <GeolocateControl
+                    style={geolocateStyle}
+                    positionOptions={{ enableHighAccuracy: true }}
+                    trackUserLocation={true}
+                />
             </MapGL>
         </div>
     )
