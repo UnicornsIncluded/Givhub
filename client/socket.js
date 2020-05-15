51735fcd5796fe2de047570d585f3f5e9284589d
Sarah Park <sarahyaepark@gmail.com>
2020-05-15 11:16:29 -0700

    Feature: Sockets update correctly

--- client/socket.js
+++ client/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
-import { linkedUserUpdated } from './store/reducers/linkedUser'
-import { updateCourierLinkedDonor } from './store/thunks/user'
+import { attemptGetLinkedUser, attemptGetUser } from './store/thunks/user'
+import { updateCourierLinkedDonor,attemptUpdateUser } from './store/thunks/user'
 import store from './store'
 const socket = io(window.location.origin)
 
@@ -10,13 +10,16 @@ socket.on('connect', () => {
 // grabbing the emit from serverside
 socket.on('clicked', (data) => {
   // can put whatever front end logic we need   
-  store.dispatch(linkedUserUpdated(data))
+  console.log('clicked socket', data)
+  store.dispatch(attemptGetLinkedUser(data))
+  store.dispatch(attemptGetUser())
 })
 
 socket.on('delivered', (linkedUserId) => {
   // can put whatever front end logic we need  
   console.log("SOCKET", linkedUserId)
   store.dispatch(updateCourierLinkedDonor(linkedUserId))
+  store.dispatch(attemptUpdateUser({ linkedUser: null }))
 })
 
 export default socket
